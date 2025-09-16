165d2a47caecc232375c0d58b6306bb4b3b5d16a
Felipe Costa <felipe.sccosta@ufape.edu.br>
2025-09-16 16:43:51 -0300

    feat(frontend): criar AuthGuard e proteger rota do diário

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DiaryListComponent } from './pages/diary-list/diary-list.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     // Rota padrão: redireciona para a lista do diário
@@ -14,5 +15,5 @@ export const routes: Routes = [
     { path: 'cadastro', component: RegisterComponent },
 
     // Rota para a página principal do diário
-    { path: 'diario', component: DiaryListComponent }
+    { path: 'diario', component: DiaryListComponent, canActivate: [authGuard] }
 ];
